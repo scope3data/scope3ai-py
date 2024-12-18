c230c9460a8534930c38ab3adb80f756e78146a4
Kevin Guevara <34045043+kevdevg@users.noreply.github.com>
2024-12-18 11:15:33 -0500

    feat: Hugging face chat and image generation first iteration (#7)
    * feat: initial pytest implementation for openai
    
    (still use scope3 api)
    
    * ci: include pytest with python matrix
    
    * feat: autogen types from openapi scope3ai yaml
    
    * fix: adding more libraries
    
    * feat: huggingface_hub tracer
    
    * feat: hugging face image generation
    
    * fix: update directory
    
    * fix: update directory
    
    * feat: huggingface text-to-image tracer
    
    * fix: .gitignore updated
    
    * fix: Files removed from .gitignore
    
    * fix: add .idea to global gitignore
    
    * fix: fix output images for text_to_image task
    
    ---------
    
    Co-authored-by: Mathieu Virbel <mat@meltingrocks.com>

---
+++ scope3ai/tracers/huggingface/text_to_image.py
@@ -0,0 +1,59 @@
+import time
+import tiktoken
+from dataclasses import dataclass
+from typing import Any, Callable, Optional
+
+from huggingface_hub import InferenceClient  # type: ignore[import-untyped]
+from huggingface_hub import TextToImageOutput as _TextToImageOutput
+
+from scope3ai.api.types import Scope3AIContext, Model, ImpactRow
+from scope3ai.api.typesgen import Task
+from scope3ai.lib import Scope3AI
+
+PROVIDER = "huggingface_hub"
+
+
+@dataclass
+class TextToImageOutput(_TextToImageOutput):
+    scope3ai: Optional[Scope3AIContext] = None
+
+
+def huggingface_text_to_image_wrapper_non_stream(
+    wrapped: Callable, instance: InferenceClient, args: Any, kwargs: Any
+) -> TextToImageOutput:
+    timer_start = time.perf_counter()
+    response = wrapped(*args, **kwargs)
+    request_latency = time.perf_counter() - timer_start
+    if kwargs.get("model"):
+        model_requested = kwargs.get("model")
+        model_used = kwargs.get("model")
+    else:
+        recommended_model = instance.get_recommended_model("text-to-image")
+        model_requested = recommended_model
+        model_used = recommended_model
+    encoder = tiktoken.get_encoding("cl100k_base")
+    if len(args) > 0:
+        prompt = args[0]
+    else:
+        prompt = kwargs["prompt"]
+    input_tokens = len(encoder.encode(prompt))
+    width, height = response.size
+    scope3_row = ImpactRow(
+        model=Model(id=model_requested),
+        model_used=Model(id=model_used),
+        input_tokens=input_tokens,
+        task=Task.text_to_image,
+        output_images=["{width}x{height}".format(width=width, height=height)],
+        request_duration_ms=request_latency
+        * 1000,  # TODO: can we get the header that has the processing time
+        managed_service_id=PROVIDER,
+    )
+
+    scope3_ctx = Scope3AI.get_instance().submit_impact(scope3_row)
+    return TextToImageOutput(response, scope3ai=scope3_ctx)
+
+
+def huggingface_text_to_image_wrapper(
+    wrapped: Callable, instance: InferenceClient, args: Any, kwargs: Any
+) -> TextToImageOutput:
+    return huggingface_text_to_image_wrapper_non_stream(wrapped, instance, args, kwargs)
