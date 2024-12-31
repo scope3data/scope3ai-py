1497a9e6f0f2e72f8b5aef8855e44dba1f348eb1
Mathieu Virbel <mat@meltingrocks.com>
2024-12-31 15:25:45 -0600

    fix(openai): fix async report and add unit tests (#39)

--- tests/test_openai_tracer.py
+++ tests/test_openai_tracer.py
@@ -1,9 +1,10 @@
 import pytest
-from openai import OpenAI
 
 
 @pytest.mark.vcr
 def test_openai_chat(tracer_init):
+    from openai import OpenAI
+
     client = OpenAI()
     response = client.chat.completions.create(
         model="gpt-4", messages=[{"role": "user", "content": "Hello World!"}]
@@ -17,6 +18,8 @@ def test_openai_chat(tracer_init):
 
 @pytest.mark.vcr
 def test_openai_chat_with_response(tracer_with_sync_init):
+    from openai import OpenAI
+
     client = OpenAI()
     response = client.chat.completions.create(
         model="gpt-4", messages=[{"role": "user", "content": "Hello World!"}]
@@ -30,3 +33,54 @@ def test_openai_chat_with_response(tracer_with_sync_init):
     assert response.scope3ai.impact.total_impact.usage_energy_wh > 0
     assert response.scope3ai.impact.total_impact.usage_emissions_gco2e > 0
     assert response.scope3ai.impact.total_impact.embodied_emissions_gco2e > 0
+
+
+@pytest.mark.vcr
+def test_openai_chat_stream(tracer_init):
+    from openai import OpenAI
+
+    client = OpenAI()
+
+    with tracer_init.trace() as tracer:
+        response = client.chat.completions.create(
+            model="gpt-4",
+            messages=[{"role": "user", "content": "Hello World!"}],
+            stream=True,
+        )
+
+        for event in response:
+            if not event.choices:
+                continue
+            print(event.choices[0].delta.content, end="", flush=True)
+        print()
+
+        impact = tracer.impact()
+        assert impact.total_energy_wh > 0
+        assert impact.total_gco2e > 0
+        assert impact.total_mlh2o > 0
+
+
+@pytest.mark.vcr
+@pytest.mark.asyncio
+async def test_openai_chat_stream_async(tracer_init):
+    from openai import AsyncOpenAI
+
+    client = AsyncOpenAI()
+
+    with tracer_init.trace() as tracer:
+        response = await client.chat.completions.create(
+            model="gpt-4",
+            messages=[{"role": "user", "content": "Hello World!"}],
+            stream=True,
+        )
+
+        async for event in response:
+            if not event.choices:
+                continue
+            print(event.choices[0].delta.content, end="", flush=True)
+        print()
+
+        impact = tracer.impact()
+        assert impact.total_energy_wh > 0
+        assert impact.total_gco2e > 0
+        assert impact.total_mlh2o > 0
