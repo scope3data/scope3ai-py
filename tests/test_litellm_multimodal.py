9018fd0c71785eb9ea8e832b957146aebd4d511f
Kevin Guevara <kevinguevaradevia@gmail.com>
2025-01-17 16:17:51 -0500

    fix: fix missing tests for litellm

--- tests/test_litellm_multimodal.py
+++ tests/test_litellm_multimodal.py
@@ -1,6 +1,5 @@
 import base64
 from pathlib import Path
-
 import pytest
 import litellm
 
@@ -67,129 +66,129 @@ def test_litellm_multimodal_vision(tracer_with_sync_init):
     assert response.scope3ai.impact.total_impact.embodied_water_ml > 0
 
 
-# @pytest.mark.vcr
-# def test_litellm_multimodal_vision_2_images(tracer_with_sync_init):
-#     from scope3ai.api.typesgen import Image
-#
-#     response = completion(
-#         model="gpt-4o-mini",
-#         messages=[
-#             {
-#                 "role": "user",
-#                 "content": [
-#                     {
-#                         "type": "text",
-#                         "text": "Hello World! What's the image about ?",
-#                     },
-#                     {
-#                         "type": "image_url",
-#                         "image_url": {
-#                             "url": load_image_b64(TEST_IMAGE_JPG),
-#                         },
-#                     },
-#                     {
-#                         "type": "image_url",
-#                         "image_url": {
-#                             "url": load_image_b64(TEST_IMAGE_PNG),
-#                         },
-#                     },
-#                 ],
-#             },
-#         ],
-#     )
-#     assert len(response.choices) > 0
-#     assert getattr(response, "scope3ai") is not None
-#     assert response.scope3ai.request.input_tokens == 34016
-#     assert response.scope3ai.request.output_tokens == 47
-#     assert response.scope3ai.request.input_images == [
-#         Image(root="512x512"),
-#         Image(root="1024x1024"),
-#     ]
-#     assert response.scope3ai.impact is not None
-#     assert response.scope3ai.impact.total_impact is not None
-#     assert response.scope3ai.impact.total_impact.usage_energy_wh > 0
-#     assert response.scope3ai.impact.total_impact.usage_emissions_gco2e > 0
-#     assert response.scope3ai.impact.total_impact.usage_water_ml > 0
-#     assert response.scope3ai.impact.total_impact.embodied_emissions_gco2e > 0
-#     assert response.scope3ai.impact.total_impact.embodied_water_ml > 0
-#
-#
-# @pytest.mark.vcr
-# def test_litellm_multimodal_audio(tracer_with_sync_init):
-#     response = completion(
-#         model="gpt-4o-audio-preview",
-#         messages=[
-#             {
-#                 "role": "user",
-#                 "content": [
-#                     {
-#                         "type": "text",
-#                         "text": "What's the audio about ?",
-#                     },
-#                     {
-#                         "type": "input_audio",
-#                         "input_audio": {
-#                             "data": file_as_b64str(TEST_AUDIO_MP3),
-#                             "format": "mp3",
-#                         },
-#                     },
-#                 ],
-#             },
-#         ],
-#     )
-#     assert len(response.choices) > 0
-#     assert getattr(response, "scope3ai") is not None
-#     assert response.scope3ai.request.input_tokens == 29
-#     assert response.scope3ai.request.output_tokens == 15
-#     assert response.scope3ai.request.input_audio_seconds >= 1
-#     assert response.scope3ai.impact is not None
-#     assert response.scope3ai.impact.total_impact is not None
-#     assert response.scope3ai.impact.total_impact.usage_energy_wh > 0
-#     assert response.scope3ai.impact.total_impact.usage_emissions_gco2e > 0
-#     assert response.scope3ai.impact.total_impact.usage_water_ml > 0
-#     assert response.scope3ai.impact.total_impact.embodied_emissions_gco2e > 0
-#     assert response.scope3ai.impact.total_impact.embodied_water_ml > 0
-#
-#
-# @pytest.mark.vcr
-# def test_litellm_multimodal_audio_2(tracer_with_sync_init):
-#     response = completion(
-#         model="gpt-4o-audio-preview",
-#         messages=[
-#             {
-#                 "role": "user",
-#                 "content": [
-#                     {
-#                         "type": "text",
-#                         "text": "What's the audio about ?",
-#                     },
-#                     {
-#                         "type": "input_audio",
-#                         "input_audio": {
-#                             "data": file_as_b64str(TEST_AUDIO_MP3),
-#                             "format": "mp3",
-#                         },
-#                     },
-#                     {
-#                         "type": "input_audio",
-#                         "input_audio": {
-#                             "data": file_as_b64str(TEST_AUDIO_WAV),
-#                             "format": "wav",
-#                         },
-#                     },
-#                 ],
-#             },
-#         ],
-#     )
-#     assert len(response.choices) > 0
-#     assert getattr(response, "scope3ai") is not None
-#     assert response.scope3ai.request.input_tokens == 46
-#     assert response.scope3ai.request.output_tokens == 17
-#     assert response.scope3ai.request.input_audio_seconds >= 1
-#     assert response.scope3ai.impact is not None
-#     assert response.scope3ai.impact.total_impact is not None
-#     assert response.scope3ai.impact.total_impact.usage_energy_wh > 0
-#     assert response.scope3ai.impact.total_impact.usage_emissions_gco2e > 0
-#     assert response.scope3ai.impact.total_impact.usage_water_ml > 0
-#     assert response.scope3ai.impact.total_impact.embodied_emissions_gco2e > 0
-#     assert response.scope3ai.impact.total_impact.embodied_water_ml > 0
+@pytest.mark.vcr
+def test_litellm_multimodal_vision_2_images(tracer_with_sync_init):
+    from scope3ai.api.typesgen import Image
+
+    response = litellm.completion(
+        model="gpt-4o-mini",
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": "Hello World! What's the image about ?",
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": load_image_b64(TEST_IMAGE_JPG),
+                        },
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": load_image_b64(TEST_IMAGE_PNG),
+                        },
+                    },
+                ],
+            },
+        ],
+    )
+    assert len(response.choices) > 0
+    assert getattr(response, "scope3ai") is not None
+    assert response.scope3ai.request.input_tokens == 1082
+    assert response.scope3ai.request.output_tokens == 1136
+    assert response.scope3ai.request.input_images == [
+        Image(root="512x512"),
+        Image(root="1024x1024"),
+    ]
+    assert response.scope3ai.impact is not None
+    assert response.scope3ai.impact.total_impact is not None
+    assert response.scope3ai.impact.total_impact.usage_energy_wh > 0
+    assert response.scope3ai.impact.total_impact.usage_emissions_gco2e > 0
+    assert response.scope3ai.impact.total_impact.usage_water_ml > 0
+    assert response.scope3ai.impact.total_impact.embodied_emissions_gco2e > 0
+    assert response.scope3ai.impact.total_impact.embodied_water_ml > 0
+
+
+@pytest.mark.vcr
+def test_litellm_multimodal_audio(tracer_with_sync_init):
+    response = litellm.completion(
+        model="gpt-4o-audio-preview",
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": "What's the audio about ?",
+                    },
+                    {
+                        "type": "input_audio",
+                        "input_audio": {
+                            "data": file_as_b64str(TEST_AUDIO_MP3),
+                            "format": "mp3",
+                        },
+                    },
+                ],
+            },
+        ],
+    )
+    assert len(response.choices) > 0
+    assert getattr(response, "scope3ai") is not None
+    assert response.scope3ai.request.input_tokens == 29
+    assert response.scope3ai.request.output_tokens == 39
+    assert response.scope3ai.request.input_audio_seconds >= 1
+    assert response.scope3ai.impact is not None
+    assert response.scope3ai.impact.total_impact is not None
+    assert response.scope3ai.impact.total_impact.usage_energy_wh > 0
+    assert response.scope3ai.impact.total_impact.usage_emissions_gco2e > 0
+    assert response.scope3ai.impact.total_impact.usage_water_ml > 0
+    assert response.scope3ai.impact.total_impact.embodied_emissions_gco2e > 0
+    assert response.scope3ai.impact.total_impact.embodied_water_ml > 0
+
+
+@pytest.mark.vcr
+def test_litellm_multimodal_audio_2(tracer_with_sync_init):
+    response = litellm.completion(
+        model="gpt-4o-audio-preview",
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": "What's the audio about ?",
+                    },
+                    {
+                        "type": "input_audio",
+                        "input_audio": {
+                            "data": file_as_b64str(TEST_AUDIO_MP3),
+                            "format": "mp3",
+                        },
+                    },
+                    {
+                        "type": "input_audio",
+                        "input_audio": {
+                            "data": file_as_b64str(TEST_AUDIO_WAV),
+                            "format": "wav",
+                        },
+                    },
+                ],
+            },
+        ],
+    )
+    assert len(response.choices) > 0
+    assert getattr(response, "scope3ai") is not None
+    assert response.scope3ai.request.input_tokens == 46
+    assert response.scope3ai.request.output_tokens == 81
+    assert response.scope3ai.request.input_audio_seconds >= 1
+    assert response.scope3ai.impact is not None
+    assert response.scope3ai.impact.total_impact is not None
+    assert response.scope3ai.impact.total_impact.usage_energy_wh > 0
+    assert response.scope3ai.impact.total_impact.usage_emissions_gco2e > 0
+    assert response.scope3ai.impact.total_impact.usage_water_ml > 0
+    assert response.scope3ai.impact.total_impact.embodied_emissions_gco2e > 0
+    assert response.scope3ai.impact.total_impact.embodied_water_ml > 0
--- tests/test_litellm_tracer.py
+++ tests/test_litellm_tracer.py
@@ -1,8 +1,5 @@
 import litellm
-import os
 import pytest
-
-os.environ["HF_TOKEN"] = "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"
 
 
 @pytest.mark.vcr
@@ -24,58 +21,57 @@ def test_litellm_chat(tracer_with_sync_init):
     assert response.scope3ai.impact.total_impact.embodied_water_ml > 0
 
 
-#
-# @pytest.mark.vcr
-# @pytest.mark.asyncio
-# async def test_litellm_async_chat(tracer_with_sync_init):
-#     response = await litellm.acompletion(
-#         messages=[{"role": "user", "content": "Hello World!"}],
-#         model="command-r",
-#     )
-#     assert len(response.choices) > 0
-#     assert getattr(response, "scope3ai") is not None
-#     assert response.scope3ai.request.input_tokens == 3
-#     assert response.scope3ai.impact is not None
-#     assert response.scope3ai.impact.total_impact is not None
-#     assert response.scope3ai.impact.total_impact.usage_energy_wh > 0
-#     assert response.scope3ai.impact.total_impact.usage_emissions_gco2e > 0
-#     assert response.scope3ai.impact.total_impact.usage_water_ml > 0
-#     assert response.scope3ai.impact.total_impact.embodied_emissions_gco2e > 0
-#     assert response.scope3ai.impact.total_impact.embodied_water_ml > 0
-#
-#
-# @pytest.mark.vcr
-# def test_litellm_stream_chat(tracer_with_sync_init):
-#     stream = litellm.completion(
-#         messages=[{"role": "user", "content": "Hello World!"}],
-#         model="claude-3-5-sonnet-20240620",
-#         stream=True,
-#     )
-#     for chunk in stream:
-#         assert getattr(chunk, "scope3ai") is not None
-#         assert chunk.scope3ai.impact is not None
-#         assert chunk.scope3ai.impact.total_impact is not None
-#         assert chunk.scope3ai.impact.total_impact.usage_energy_wh > 0
-#         assert chunk.scope3ai.impact.total_impact.usage_emissions_gco2e > 0
-#         assert chunk.scope3ai.impact.total_impact.usage_water_ml > 0
-#         assert chunk.scope3ai.impact.total_impact.embodied_emissions_gco2e > 0
-#         assert chunk.scope3ai.impact.total_impact.embodied_water_ml > 0
-#
-#
-# @pytest.mark.vcr
-# @pytest.mark.asyncio
-# async def test_litellm_async_stream_chat(tracer_with_sync_init):
-#     stream = await litellm.acompletion(
-#         messages=[{"role": "user", "content": "Hello World!"}],
-#         model="claude-3-5-sonnet-20240620",
-#         stream=True,
-#     )
-#     async for chunk in stream:
-#         assert getattr(chunk, "scope3ai") is not None
-#         assert chunk.scope3ai.impact is not None
-#         assert chunk.scope3ai.impact.total_impact is not None
-#         assert chunk.scope3ai.impact.total_impact.usage_energy_wh > 0
-#         assert chunk.scope3ai.impact.total_impact.usage_emissions_gco2e > 0
-#         assert chunk.scope3ai.impact.total_impact.usage_water_ml > 0
-#         assert chunk.scope3ai.impact.total_impact.embodied_emissions_gco2e > 0
-#         assert chunk.scope3ai.impact.total_impact.embodied_water_ml > 0
+@pytest.mark.vcr
+@pytest.mark.asyncio
+async def test_litellm_async_chat(tracer_with_sync_init):
+    response = await litellm.acompletion(
+        messages=[{"role": "user", "content": "Hello World!"}],
+        model="command-r",
+    )
+    assert len(response.choices) > 0
+    assert getattr(response, "scope3ai") is not None
+    assert response.scope3ai.request.input_tokens == 3
+    assert response.scope3ai.impact is not None
+    assert response.scope3ai.impact.total_impact is not None
+    assert response.scope3ai.impact.total_impact.usage_energy_wh > 0
+    assert response.scope3ai.impact.total_impact.usage_emissions_gco2e > 0
+    assert response.scope3ai.impact.total_impact.usage_water_ml > 0
+    assert response.scope3ai.impact.total_impact.embodied_emissions_gco2e > 0
+    assert response.scope3ai.impact.total_impact.embodied_water_ml > 0
+
+
+@pytest.mark.vcr
+def test_litellm_stream_chat(tracer_with_sync_init):
+    stream = litellm.completion(
+        messages=[{"role": "user", "content": "Hello World!"}],
+        model="claude-3-5-sonnet-20240620",
+        stream=True,
+    )
+    for chunk in stream:
+        assert getattr(chunk, "scope3ai") is not None
+        assert chunk.scope3ai.impact is not None
+        assert chunk.scope3ai.impact.total_impact is not None
+        assert chunk.scope3ai.impact.total_impact.usage_energy_wh > 0
+        assert chunk.scope3ai.impact.total_impact.usage_emissions_gco2e > 0
+        assert chunk.scope3ai.impact.total_impact.usage_water_ml > 0
+        assert chunk.scope3ai.impact.total_impact.embodied_emissions_gco2e > 0
+        assert chunk.scope3ai.impact.total_impact.embodied_water_ml > 0
+
+
+@pytest.mark.vcr
+@pytest.mark.asyncio
+async def test_litellm_async_stream_chat(tracer_with_sync_init):
+    stream = await litellm.acompletion(
+        messages=[{"role": "user", "content": "Hello World!"}],
+        model="claude-3-5-sonnet-20240620",
+        stream=True,
+    )
+    async for chunk in stream:
+        assert getattr(chunk, "scope3ai") is not None
+        assert chunk.scope3ai.impact is not None
+        assert chunk.scope3ai.impact.total_impact is not None
+        assert chunk.scope3ai.impact.total_impact.usage_energy_wh > 0
+        assert chunk.scope3ai.impact.total_impact.usage_emissions_gco2e > 0
+        assert chunk.scope3ai.impact.total_impact.usage_water_ml > 0
+        assert chunk.scope3ai.impact.total_impact.embodied_emissions_gco2e > 0
+        assert chunk.scope3ai.impact.total_impact.embodied_water_ml > 0
