b81291dda3e7b326db9472a9d286575d0a214962
Kevin Guevara <kevinguevaradevia@gmail.com>
2025-01-30 18:16:18 -0500

    fix: remove comment

--- tests/test_gemini_tracer.py
+++ tests/test_gemini_tracer.py
@@ -45,7 +45,7 @@ async def test_gemini_chat_stream_async(tracer_with_sync_init):
 
 # @pytest.mark.vcr
 # def test_gemini_text_to_image(tracer_with_sync_init):
-#     client = genai.Client(api_key='AIzaSyAKbyvXpC1Dm--J1GkJeOZUal63DE-q4TE')
+#     client = genai.Client(api_key='GEMINI_API_KEY')
 #     response = client.models.generate_images(model='imagen-3.0-generate-002', prompt='Fuzzy bunnies in my kitchen',
 #     config=types.GenerateImagesConfig(
 #         negative_prompt= 'people',
@@ -58,7 +58,7 @@ async def test_gemini_chat_stream_async(tracer_with_sync_init):
 # @pytest.mark.vcr
 # @pytest.mark.asyncio
 # async def test_gemini_async_chat(tracer_with_sync_init):
-#     model = genai.GenerativeModel(api_key='AIzaSyAKbyvXpC1Dm--J1GkJeOZUal63DE-q4TE', model='gemini-1.5-flash')
+#     model = genai.GenerativeModel(api_key='GEMINI_API_KEY', model='gemini-1.5-flash')
 #     response = await model.generate_content_async(contents='How does RLHF work?')
 #     print(response.text)
 
